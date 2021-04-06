303eba0449fd648a77eaa60f49933e01800e8d94
Jessica Brand <jeshbrand@gmail.com>
2021-04-06 16:54:51 +0100

    finished chapter 6 - included top up limit' '

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,23 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :max_balance
+  DEFAULT_BALANCE = 30
+  MAX_BALANCE = 90
 
-  def initialize
-    @balance = 100
+  def initialize()
+    @balance = DEFAULT_BALANCE
+    @max_balance = MAX_BALANCE
   end
+
+  def top_up(sum)
+    #fail "stormy conditions" if @weather.stormy?
+    fail "max balance of #{max_balance} exceeded" if balance_exceeded?(sum)
+    @balance += sum
+  end
+
+  private
+
+  def balance_exceeded?(sum)
+    @balance+sum > @max_balance
+  end
+
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,9 +1,35 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  let (:default_balance) {Oystercard::DEFAULT_BALANCE}
+  let (:max_balance) {Oystercard::MAX_BALANCE}
+  # let (:test_plane) { Plane.new }
   describe '#balance' do
     it 'responds' do
       expect(subject).to respond_to(:balance)
     end
+
+    it 'returns default balance' do
+      expect(subject.balance).to eq(default_balance)
+    end
+  end
+
+  describe '#top_up' do
+    it 'responds' do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+
+    it 'top up increases balance' do
+      sum = 10
+      subject.top_up(sum)
+      expect(subject.balance).to eq(default_balance+sum)
+    end
+
+    it 'top up increases balance' do
+      sum = max_balance
+      expect { subject.top_up(sum) }.to raise_error("max balance of #{max_balance} exceeded")
+    end
+
   end
 end
