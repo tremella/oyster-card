7e417ea0e95a7b7f5492c2b9b6f077ab19b3b30d
Jessica Brand <jeshbrand@gmail.com>
2021-04-07 15:46:55 +0100

    up to part 9

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,17 +1,29 @@
 class Oystercard
-  attr_reader :balance, :max_balance
+  attr_reader :balance, :max_balance, :in_journey
   DEFAULT_BALANCE = 30
   MAX_BALANCE = 90
 
   def initialize()
     @balance = DEFAULT_BALANCE
     @max_balance = MAX_BALANCE
+    @in_journey = false
   end
 
   def top_up(sum)
-    #fail "stormy conditions" if @weather.stormy?
     fail "max balance of #{max_balance} exceeded" if balance_exceeded?(sum)
     @balance += sum
+  end
+
+  def deduct(sum)
+    @balance -= sum
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
   private
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -15,21 +15,69 @@ describe Oystercard do
     end
   end
 
+  describe '#deduct' do
+    it 'responds' do
+      expect(subject).to respond_to(:deduct).with(1).argument
+    end
+
+    it '#deduct reduces balance' do
+      expect { subject.deduct 20 }.to change {subject.balance }.by -20
+    end
+  end
+
   describe '#top_up' do
     it 'responds' do
       expect(subject).to respond_to(:top_up).with(1).argument
     end
 
-    it 'top up increases balance' do
-      sum = 10
-      subject.top_up(sum)
-      expect(subject.balance).to eq(default_balance+sum)
+    it '#top up increases balance' do
+      ## sum = 10
+      ## subject.top_up(sum)
+      ## expect(subject.balance).to eq(default_balance+sum)
+      # more elegant alternative:
+      expect { subject.top_up 20 }.to change {subject.balance }.by 20
     end
 
-    it 'top up increases balance' do
+    it '#top up will not permit exceeding max balance' do
       sum = max_balance
       expect { subject.top_up(sum) }.to raise_error("max balance of #{max_balance} exceeded")
     end
 
   end
+
+  describe '#in_journey' do
+    it 'will respond to #in_journey' do
+      expect(subject).to respond_to(:in_journey)
+    end
+
+    it 'by default, is not in a journey' do
+      expect(subject.in_journey).to eq(false)
+      # expect(subject).to be_in_journey
+      # revisit walkthrough to try this way
+    end
+  end
+
+  describe '#touch_in' do
+    it 'will respond to #touch_in' do
+      expect(subject).to respond_to(:touch_in)
+    end
+
+    it 'will change #in_journey to true' do
+      subject.touch_in
+      expect(subject.in_journey).to eq(true)
+    end
+
+  end
+  describe '#touch_out' do
+    it 'will respond to #touch_out' do
+      expect(subject).to respond_to(:touch_out)
+    end
+
+    it 'will change #in_journey to true' do
+      subject.touch_out
+      expect(subject.in_journey).to eq(false)
+    end
+
+  end
+
 end
