0a5e9a62fe261c3da18f32b3a76e495674d31c5b
ian-1 <ian@portmoor.com>
2021-04-07 14:31:37 +0100

    add reduce method to card

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,17 +1,21 @@
 class Oystercard
   attr_reader :balance, :max_balance
-  DEFAULT_BALANCE = 30
+
+  DEFAULT_BALANCE = 0
   MAX_BALANCE = 90
 
-  def initialize()
+  def initialize
     @balance = DEFAULT_BALANCE
     @max_balance = MAX_BALANCE
   end
 
   def top_up(sum)
-    #fail "stormy conditions" if @weather.stormy?
     fail "max balance of #{max_balance} exceeded" if balance_exceeded?(sum)
     @balance += sum
+  end
+
+  def deduct(sum)
+    @balance -= sum
   end
 
   private
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -2,8 +2,8 @@ require 'oystercard'
 
 describe Oystercard do
 
-  let (:default_balance) {Oystercard::DEFAULT_BALANCE}
-  let (:max_balance) {Oystercard::MAX_BALANCE}
+  let (:default_balance) { Oystercard::DEFAULT_BALANCE }
+  let (:max_balance) { Oystercard::MAX_BALANCE }
   # let (:test_plane) { Plane.new }
   describe '#balance' do
     it 'responds' do
@@ -23,13 +23,21 @@ describe Oystercard do
     it 'top up increases balance' do
       sum = 10
       subject.top_up(sum)
-      expect(subject.balance).to eq(default_balance+sum)
+      expect(subject.balance).to eq(default_balance + sum)
     end
 
-    it 'top up increases balance' do
-      sum = max_balance
+    it 'raises error if exceeds max balance' do
+      sum = max_balance + 1
       expect { subject.top_up(sum) }.to raise_error("max balance of #{max_balance} exceeded")
     end
+  end
 
+  describe '#deduct' do
+    it { expect(subject).to respond_to(:deduct).with(1).argument }
+
+    it 'reduces balance' do
+      sum = 30
+      expect(subject.deduct(sum)).to eq(default_balance - sum)
+    end
   end
 end
